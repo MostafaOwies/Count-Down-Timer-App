6db98f09e3967102986203fca932f9246a79f1a9
MostafaOwies <mostafaowies@yahoo.com>
2021-08-17 20:58:44 +0200

    3rd Commit

--- app/src/main/java/com/example/timerapp/MainActivity.java
+++ app/src/main/java/com/example/timerapp/MainActivity.java
@@ -5,12 +5,15 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.os.CountDownTimer;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.SeekBar;
 import android.widget.TextView;
 
 import org.w3c.dom.Text;
+
+import static android.content.ContentValues.TAG;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -22,30 +25,42 @@ public class MainActivity extends AppCompatActivity {
     String secsString;
     Button button;
     MediaPlayer mediaPlayer;
-    int x = 1;
+    CountDownTimer countDownTimer;
+    boolean counter = false;
 
     public void alarm(View view){
 
 
 
-        if ( x ==1) {
+        if ( counter==false) {
+            counter = true;
+            seekbar.setEnabled(false);
              button = findViewById(R.id.goButton);
             button.setText("Stop !");
-            new CountDownTimer(seekbar.getProgress() * 1000, 1000) {
+             countDownTimer=new CountDownTimer(seekbar.getProgress() * 1000+100, 1000) {
                 public void onTick(long l) {
                     timerFunction((int) l / 1000);
                 }
 
                 public void onFinish() {
+                    mediaPlayer=MediaPlayer.create(getApplicationContext(),R.raw.awaken);
                     mediaPlayer.start();
+                    Log.i(TAG, "onFinish: finshed");
                 }
             }.start();
-            x =2;
+
         }
-        else if (x==2){
-            x=1;
+        else if (counter==true){
+            counter=false;
+            seekbar.setEnabled(true);
             button.setText("GO !");
-            
+            mediaPlayer.stop();
+            countDownTimer.cancel();
+            seekbar.setProgress(30);
+            textView.setText("0 : 30");
+
+        //    mediaPlayer= MediaPlayer.create(this,R.raw.awaken);
+
         }
 
 
@@ -68,12 +83,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        mediaPlayer= MediaPlayer.create(this,R.raw.awaken);
+      //  mediaPlayer= MediaPlayer.create(this,R.raw.awaken);
 
+        int progress = 30;
          seekbar = findViewById(R.id.timerseekBar);
          textView = findViewById(R.id.timerCountDown);
         seekbar.setMax(600);
-        seekbar.setProgress(0);
+        seekbar.setProgress(progress);
+
 
         seekbar.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
             @Override
